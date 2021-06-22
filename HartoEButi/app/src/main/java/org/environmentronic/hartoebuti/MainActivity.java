8e871cee9c45f4736d29b80292cae8988a9549ce
RusvelLeyva <71774981+RusvelLeyva@users.noreply.github.com>
2021-06-22 16:04:51 -0500

    Pedidos Básicos

--- HartoEButi/app/src/main/java/org/environmentronic/hartoebuti/MainActivity.java
+++ HartoEButi/app/src/main/java/org/environmentronic/hartoebuti/MainActivity.java
@@ -2,13 +2,19 @@ package org.environmentronic.hartoebuti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //for changing status bar icon colors
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         setContentView(R.layout.activity_main);
     }
 }
