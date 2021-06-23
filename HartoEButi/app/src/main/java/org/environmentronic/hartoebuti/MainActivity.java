aa9755c5ef369b67bb3e7fbf5f69480255ca1856
RusvelLeyva <71774981+RusvelLeyva@users.noreply.github.com>
2021-06-23 10:05:34 -0500

    acciones

--- HartoEButi/app/src/main/java/org/environmentronic/hartoebuti/MainActivity.java
+++ HartoEButi/app/src/main/java/org/environmentronic/hartoebuti/MainActivity.java
@@ -5,8 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ExtendedFloatingActionButton addFloatingInfo;
+    private ExtendedFloatingActionButton extendedFloatingActionButton;
+    private Boolean isAllFabsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +24,33 @@ public class MainActivity extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
         }
         setContentView(R.layout.activity_main);
+
+        // casting
+        extendedFloatingActionButton = (ExtendedFloatingActionButton) findViewById(R.id.extendedFloatingActionButton);
+        addFloatingInfo = (ExtendedFloatingActionButton) findViewById(R.id.floatingInfo);
+        // ocultamos el boton de info
+        addFloatingInfo.setVisibility(View.GONE);
+        isAllFabsVisible = false;
+
+        extendedFloatingActionButton.shrink();
+
+        extendedFloatingActionButton.setOnClickListener(v -> {
+            if (!isAllFabsVisible) {
+                addFloatingInfo.show();
+                extendedFloatingActionButton.extend();
+                isAllFabsVisible = true;
+            } else {
+                addFloatingInfo.hide();
+                extendedFloatingActionButton.shrink();
+                isAllFabsVisible = false;
+            }
+        });
+
+        addFloatingInfo.setOnClickListener(
+                view -> Toast.makeText
+                        (MainActivity
+                                        .this, "Información del desarrollador",
+                                Toast.LENGTH_SHORT).show());
+
     }
 }
